971417cc1eb6f48fabdcc8bcdf3d10fead1c7f02
Kirsty Scott-Brown <K.Scott-Brown@albynschool.org>
2025-03-26 15:31:52 +0000

    hi

--- traversing1-Darraystask1.py
+++ traversing1-Darraystask1.py
@@ -1,6 +1,6 @@
-food_weight = []
 weight = [50, 75, 250, 299, 180]
+total_weight = 0
 for counter in range(0,4):
-    total_weight = weight[0] + weight[1]
-weight.append(food_weight)
+    total_weight = total_weight + weight[counter]
+
 print(total_weight)
